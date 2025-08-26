8e7f8fbbea63ee7089c6a808d6f56aafee586926
Ryan Matte <ryanmatte55@gmail.com>
2025-08-26 18:37:26 -0400

    add Vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 
 import type { Metadata } from "next";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import { Geist, Geist_Mono, Bebas_Neue } from "next/font/google";
@@ -36,6 +36,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <Navbar />
         {children}
+        <SpeedInsights />
         <Footer />
       </body>
     </html>
